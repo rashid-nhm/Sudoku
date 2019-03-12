4b92714d3acb64a7700df1b98a24b2712b980d29
Rashid NHM <rashid.nhm@email.com>
2019-03-11 23:34:01 -0400

    Made everything 0 indexed

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,25 +1,26 @@
-from typing import List, Optional, Tuple
+from collections import Sequence
+from typing import List, Optional, Union
 
 
 class _Cell:
-    __slots__ = ["__candidates", "candidates", "__value", "value"]
+    __slots__ = ["__candidates", "__value"]
 
     def __init__(self) -> None:
-        self.__candidates: List[Optional[bool]] = [None, True, True, True, True, True, True, True, True, True]
+        self.__candidates: List[bool] = [True, True, True, True, True, True, True, True, True]
         self.__value: int = 0
 
     @property
-    def candidates(self) -> Tuple[int]:
-        return tuple([candidate for candidate in range(1, len(self.__candidates)) if self[candidate]])
+    def candidates(self) -> List[bool]:
+        return self.__candidates
 
     def candidate(self, i: int) -> Optional[bool]:
         return self[i]
 
     def __getitem__(self, index: int) -> Optional[bool]:
-        return self.__candidates[index] if 0 < index <= 9 else None
+        return self.__candidates[index] if 0 <= index < 9 else None
 
     def __setitem__(self, index: int, value: bool) -> None:
-        if 0 < index <= 9:
+        if 0 <= index < 9:
             self.__candidates[index] = value
 
     def __eq__(self, other: '_Cell') -> bool:
@@ -31,11 +32,11 @@ class _Cell:
 
     @value.setter
     def value(self, value: int) -> None:
-        if not (1 < value <= 9):
-            raise ValueError("Value of a cell must be between 1-9 (inclusive)")
+        if not (1 <= value <= 9):
+            raise ValueError(f"Value of a cell must be between 1-9 (inclusive). Got {value}")
         self.__value: int = value
-        self.__candidates: List[Optional[bool]] = [None, False, False, False, False, False, False, False, False, False]
-        self.__candidates[value]: bool = True
+        self.__candidates: List[bool] = [False, False, False, False, False, False, False, False, False]
+        self.__candidates[value - 1]: bool = True
 
     def is_solved(self) -> bool:
         return self.value != 0
@@ -48,43 +49,48 @@ class _Cell:
 
 
 class _Region:
-    __slots__ = ["__cells", "cells"]
+    __slots__ = ["__cells"]
 
     def __init__(self) -> None:
-        self.__cells: List[Optional[_Cell]] = [None,
+        self.__cells: List[Optional[_Cell]] = [
                                                _Cell(), _Cell(), _Cell(),
                                                _Cell(), _Cell(), _Cell(),
-                                               _Cell(), _Cell(), _Cell()]
-
-    @property
-    def cells(self) -> List[_Cell]:
-        return self.__cells[1:]
-
-    def cell(self, i: int) -> Optional[_Cell]:
-        return self[i]
-
-    def __getitem__(self, index: int) -> Optional[_Cell]:
-        return self.__cells[index] if 0 < index <= 9 else None
-
-    def is_solved(self) -> bool:
-        return all([cell.is_solved() for cell in self.cells])
-
-
-class _Line:
-    def __init__(self,  *cells: _Cell):
-        assert len(cells) == 9, "9 cells must be passed to create a full line"
-        assert all(isinstance(cell, _Cell) for cell in cells), "All cells passed to a line must be of instance Cell"
-
-        self.__cells: List[_Cell] = cells
+                                               _Cell(), _Cell(), _Cell(),
+                                               ]
 
     @property
     def cells(self) -> List[_Cell]:
         return self.__cells
 
-    def __getitem__(self, i: int) -> Optional[_Cell]:
-        if 0 < i <= 9:
-            return self.cells[i]
-        return None
+    def cell(self, i: int) -> Optional[_Cell]:
+        return self[i]
+
+    def __getitem__(self, index: int) -> Optional[_Cell]:
+        return self.__cells[index] if 0 <= index < 9 else None
+
+    def is_solved(self) -> bool:
+        return all([cell.is_solved() for cell in self.cells])
+
+
+class _Line(Sequence):
+    def __init__(self,  *cells: _Cell):
+        assert len(cells) == 9, "9 cells must be passed to create a full line"
+        assert all(isinstance(cell, _Cell) for cell in cells), "All cells passed to a line must be of instance Cell"
+
+        self.__cells: List[_Cell] = [*cells]
+
+    @property
+    def cells(self) -> List[_Cell]:
+        return self.__cells
+
+    def cell(self, i: int) -> Optional[_Cell]:
+        return self.__cells[i] if 0 <= i < 9 else None
+
+    def __len__(self) -> int:  # Required to be implemented by Sequence parent class
+        return 9
+
+    def __getitem__(self, splice: int) -> Union[_Cell, List[_Cell]]:
+        return self.cells[splice]
 
     def __setitem__(self, i: int, value: int):
         self.__cells[i].value = value
@@ -93,73 +99,76 @@ class _Line:
         return all([cell.is_solved() for cell in self.cells])
 
 
-class _Column:
-    pass
-
-
 class Board:
     def __init__(self, file: str = None) -> None:
-        self.__regions = [None,
+        self.__regions = [
                           _Region(), _Region(), _Region(),
                           _Region(), _Region(), _Region(),
-                          _Region(), _Region(), _Region()]
+                          _Region(), _Region(), _Region(),
+                          ]
 
-        self.__rows: List[Optional[_Line]] = [None,
-                                              _Line(self[1][1], self[1][2], self[1][3], self[2][1], self[2][2], self[2][3], self[3][1], self[3][2], self[3][3]),
-                                              _Line(self[1][4], self[1][5], self[1][6], self[2][4], self[2][5], self[2][6], self[3][4], self[3][5], self[3][6]),
-                                              _Line(self[1][7], self[1][8], self[1][9], self[2][7], self[2][8], self[2][9], self[3][7], self[3][8], self[3][9]),
+        self.__rows: List[_Line] = [
+            _Line(self[0][0], self[0][1], self[0][2], self[1][0], self[1][1], self[1][2], self[2][0], self[2][1], self[2][2]),
+            _Line(self[0][3], self[0][4], self[0][5], self[1][3], self[1][4], self[1][5], self[2][3], self[2][4], self[2][5]),
+            _Line(self[0][6], self[0][7], self[0][8], self[1][6], self[1][7], self[1][8], self[2][6], self[2][7], self[2][8]),
 
-                                              _Line(self[4][1], self[4][2], self[4][3], self[5][1], self[5][2], self[5][3], self[6][1], self[6][2], self[6][3]),
-                                              _Line(self[4][4], self[4][5], self[4][6], self[5][4], self[5][5], self[5][6], self[6][4], self[6][5], self[6][6]),
-                                              _Line(self[4][7], self[4][8], self[4][9], self[5][7], self[5][8], self[5][9], self[6][7], self[6][8], self[6][9]),
+            _Line(self[3][0], self[3][1], self[3][2], self[4][0], self[4][1], self[4][2], self[5][0], self[5][1], self[5][2]),
+            _Line(self[3][3], self[3][4], self[3][5], self[4][3], self[4][4], self[4][5], self[5][3], self[5][4], self[5][5]),
+            _Line(self[3][6], self[3][7], self[3][8], self[4][6], self[4][7], self[4][8], self[5][6], self[5][7], self[5][8]),
 
-                                              _Line(self[7][1], self[7][2], self[7][3], self[8][1], self[8][2], self[8][3], self[9][1], self[9][2], self[9][3]),
-                                              _Line(self[7][4], self[7][5], self[7][6], self[8][4], self[8][5], self[8][6], self[9][4], self[9][5], self[9][6]),
-                                              _Line(self[7][7], self[7][8], self[7][9], self[8][7], self[8][8], self[8][9], self[9][7], self[9][8], self[9][9])
-                                              ]
+            _Line(self[6][0], self[6][1], self[6][2], self[7][0], self[7][1], self[7][2], self[8][0], self[8][1], self[8][2]),
+            _Line(self[6][3], self[6][4], self[6][5], self[7][3], self[7][4], self[7][7], self[8][3], self[8][4], self[8][5]),
+            _Line(self[6][6], self[6][7], self[6][8], self[7][6], self[7][7], self[7][8], self[8][6], self[8][7], self[8][8]),
+        ]
 
-        self.__columns: List[Optional[_Line]] = [None,
-                                                 _Line(self[1][1], self[1][4], self[1][7], self[4][1], self[4][4], self[4][7], self[7][1], self[7][4], self[7][7]),
-                                                 _Line(self[1][2], self[1][5], self[1][8], self[4][2], self[4][5], self[4][8], self[7][2], self[7][5], self[7][8]),
-                                                 _Line(self[1][3], self[1][6], self[1][9], self[4][3], self[4][6], self[4][9], self[7][3], self[7][6], self[7][9]),
+        self.__columns: List[_Line] = [
+            _Line(self[0][0], self[0][3], self[0][6], self[3][0], self[3][3], self[3][6], self[6][0], self[6][3], self[6][6]),
+            _Line(self[0][1], self[0][4], self[0][7], self[3][1], self[3][4], self[3][7], self[6][1], self[6][4], self[6][7]),
+            _Line(self[0][2], self[0][5], self[0][8], self[3][2], self[3][5], self[3][8], self[6][2], self[6][5], self[6][8]),
 
-                                                 _Line(self[2][1], self[2][4], self[2][7], self[5][1], self[5][4], self[5][7], self[8][1], self[8][4], self[8][7]),
-                                                 _Line(self[2][2], self[2][5], self[2][8], self[5][2], self[5][5], self[5][8], self[8][2], self[8][5], self[8][8]),
-                                                 _Line(self[2][3], self[2][6], self[2][9], self[5][3], self[5][6], self[5][9], self[8][3], self[8][6], self[8][9]),
+            _Line(self[1][0], self[1][3], self[1][6], self[4][0], self[4][3], self[4][6], self[7][0], self[7][3], self[7][6]),
+            _Line(self[1][1], self[1][4], self[1][7], self[4][1], self[4][4], self[4][7], self[7][1], self[7][4], self[7][7]),
+            _Line(self[1][2], self[1][5], self[1][8], self[4][2], self[4][6], self[4][8], self[7][2], self[7][5], self[7][8]),
 
-                                                 _Line(self[3][1], self[3][4], self[3][7], self[6][1], self[6][4], self[6][7], self[9][1], self[9][4], self[9][7]),
-                                                 _Line(self[3][2], self[3][5], self[3][8], self[6][2], self[6][5], self[6][8], self[9][2], self[9][5], self[9][8]),
-                                                 _Line(self[3][3], self[3][6], self[3][9], self[6][3], self[6][6], self[6][9], self[9][3], self[9][6], self[9][9])
-                                                 ]
+            _Line(self[2][0], self[2][3], self[2][6], self[5][0], self[5][3], self[5][6], self[8][0], self[8][3], self[8][6]),
+            _Line(self[2][1], self[2][4], self[2][7], self[5][1], self[5][4], self[5][7], self[8][1], self[8][4], self[8][7]),
+            _Line(self[2][2], self[2][5], self[2][8], self[5][2], self[5][5], self[5][8], self[8][2], self[8][5], self[8][8]),
+        ]
         if file:
             self.parse(file=file)
 
     def __getitem__(self, i: int) -> Optional[_Region]:
-        return self.__regions[i] if 0 < i <= 9 else None
+        return self.__regions[i] if 0 <= i < 9 else None
 
     @property
     def regions(self) -> List[_Region]:
-        return self.__regions[1:]
+        return self.__regions
 
     def region(self, i: int) -> Optional[_Region]:
         return self[i]
 
     @property
-    def rows(self) -> List[List[_Cell]]:
-        return self.__rows[1:]
+    def rows(self) -> List[_Line]:
+        return self.__rows
+
+    def row(self, i: int) -> _Line:
+        return self.__rows[i]
 
     @property
-    def columns(self) -> List[List[_Cell]]:
-        return self.__columns[1:]
+    def columns(self) -> List[_Line]:
+        return self.__columns
 
-    def parse(self, file: str=None) -> None:
+    def column(self, i: int) -> _Line:
+        return self.__columns[i]
+
+    def parse(self, file: str = None) -> None:
         with open(file) as sudoku_challenge:
-            for i, row in enumerate(sudoku_challenge, start=1):
+            for i, row in enumerate(sudoku_challenge):
                 row = row.strip()
-                for j, value in enumerate(row, start=1):
+                for j, value in enumerate(row):
                     value = int(value)
                     if value:
-                        self[i][j].value = value
+                        self.row(i).cell(j).value = value
 
     def __str__(self) -> str:
         ret: str = ""
@@ -191,7 +200,7 @@ class Board:
 
         # Remove candidates from same row
         for row in self.rows:
-            if all([cell.is_solved() for cell in row]):  # row.is_solved()
+            if row.is_solved():
                 continue
             for cell1 in row:
                 if cell1.value:
@@ -201,7 +210,7 @@ class Board:
 
         # Remove candidates from same column
         for column in self.columns:
-            if all([cell.is_solved() for cell in column]):  # column.is_solved()
+            if column.is_solved():
                 continue
             for cell1 in column:
                 if cell1.value:
@@ -213,7 +222,7 @@ class Board:
         for region in self.regions:
             if region.is_solved():
                 continue
-            for cell in region:
+            for cell in region.cells:
                 if not cell.value and len(cell.candidates) == 1:
                     cell.value = cell.candidates[0]
 
